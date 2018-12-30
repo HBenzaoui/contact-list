35ce1e9f3fe69a67bcd9f5302c446fdcc089a4ad
Hamza Benzaoui <hamzapdd@gmail.com>
2018-12-30 18:54:48 +0100

    Updates

---
+++ precache-manifest.c8b5c438a9d8d1bdd20c94b224e21e6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "62f6a218dba7c6531154",
+    "url": "/contact-list/static/css/main.a2093ee7.chunk.css"
+  },
+  {
+    "revision": "62f6a218dba7c6531154",
+    "url": "/contact-list/static/js/main.62f6a218.chunk.js"
+  },
+  {
+    "revision": "3e526301ada6d3730aaa",
+    "url": "/contact-list/static/css/1.5360783d.chunk.css"
+  },
+  {
+    "revision": "3e526301ada6d3730aaa",
+    "url": "/contact-list/static/js/1.3e526301.chunk.js"
+  },
+  {
+    "revision": "377b31ea3711a9d6ba50",
+    "url": "/contact-list/static/js/runtime~main.377b31ea.js"
+  },
+  {
+    "revision": "dcda253f19440fec173375c391ac9c95",
+    "url": "/contact-list/index.html"
+  }
+];
